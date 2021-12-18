b362894acc3b3a89ec59f91e751c19aa632f3f21
Ivan Yastrebov <esim.i2p@gmail.com>
2021-12-18 09:55:45 +0300

    GNU/Linux, Сб 18 дек 2021 09:55:45 MSK

--- run_once_after_10_asdf.sh
+++ run_once_after_10_asdf.sh
@@ -5,5 +5,9 @@ asdf plugin add python || true
 asdf plugin add golang || true
 asdf plugin add rust || true
 
+asdf install golang latest
+asdf global golang latest
+asdf install rust latest
+asdf global rust latest
 asdf install python latest
 asdf global python latest
